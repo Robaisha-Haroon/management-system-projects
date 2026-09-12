390ee846d4dc5c113828a61ee1a521b0c5f53ab2
Robaisha Haroon <robaishaharoon2010@gmail.com>
2026-09-13 00:08:20 +0500

    Create CRUD_Project.py
    This project can manage a simple to-do list using memory-based APIs.

---
+++ CRUD_Project.py
@@ -0,0 +1,31 @@
+%%writefile main.py 
+
+# crud project
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app= FastAPI()
+todo_list = []
+
+class Task(BaseModel):
+    id: int
+    title: str
+    is_done: bool= False
+
+@app.get("/tasks")
+def get_all_tasks():
+    return {"Tasks": todo_list}
+
+@app.post("/tasks")
+def create_tasks(new_task : Task):
+    todo_list.append(new_task)
+    return {"Message": "Task has been added successfully"}
+
+@app.put("/tasks/{task_id}")
+def update_tasks(task_id: int, updated_task: Task):
+    for i in range(len(todo_list)):
+        if todo_list[i].id == task_id:
+            todo_list[i] = updated_task
+            return {"tasks": "task updated"}
+    return {"Error": "Task not found" }
